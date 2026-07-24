930d171a0900b43c2d8365463948db3a8e8d8f16
Andhika Rahman <andhikarhmnp@gmail.com>
2026-07-25 05:41:18 +0700

    belajar string

--- main.py
+++ main.py
@@ -1,14 +1,7 @@
-import sys
+# Belajar string
 
-# 1. Objek baru dibuat
-x = ["Python", "2026"]
-print(sys.getrefcount(x))  # Output: 2 (x + argumen fungsi)
+s1 = 'Andhika Rahman Putra'
 
-# 2. Kita tambah dua variabel baru yang menunjuk ke objek yang sama
-y = x
-z = x
-print(sys.getrefcount(x))  # Output: 4 (x, y, z + argumen fungsi)
+s1 = 'p' + s1[1 : ]
 
-# 3. Kita hapus satu referensi menggunakan 'del'
-del y
-print(sys.getrefcount(x))  # Output: 3 (x, z + argumen fungsi)
+print(s1)
